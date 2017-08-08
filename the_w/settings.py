d3cda7c925a6ef47a56409f997eb82a4b6fab9f6
DarWafa <wafahssain@gmail.com>
2017-08-08 17:21:48 +0300

    prepared Django project for deployment

--- the_w/settings.py
+++ the_w/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '23!r1z%!i=n&7!q1(9f#y%nklvgd6k#7q7@=s(a*c_)h&%t+ne'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['128.199.214.238']
 
 
 # Application definition
@@ -80,10 +80,24 @@ WSGI_APPLICATION = 'the_w.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '36b0ba0f2c3a2224e371e53f9e4b5907',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -125,9 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS= [
-    os.path.join(BASE_DIR , "static")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS= [
+#     os.path.join(BASE_DIR , "static")
+# ]
 
-MEDIA_URL = '/media/'
+ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
